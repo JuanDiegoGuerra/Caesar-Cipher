84cfc7f2fab57c5050a5efecb37ff2d9822e587a
Juan Diego Guerra <jdxelric@gmail.com>
2023-04-08 22:50:56 -0500

    Update index.js
    Create printChar function

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ const inputOriginal = document.getElementById('input-original');
 const cifrador = document.getElementById('cifrador');
 const resultado = document.getElementById('resultado');
 const rango = document.getElementById('rango');
+
+const printChar = (currentLetterIndex, wordArray) => {
+    if(wordArray.length === currentLetterIndex) return;
+    inputOriginal.value = inputOriginal.value.substring(1)
+    const spanChar = document.createElement("span");
+    resultado.appendChild(spanChar);
+    animateChar(spanChar)
+        .then( () => {
+            const charSinCodificar = wordArray[currentLetterIndex];
+            spanChar.innerHTML = alfabeto.includes(charSinCodificar) ? 
+                alfabeto[(alfabeto.indexOf(charSinCodificar) + parseInt(rango.value)) % alfabeto.length] : 
+                charSinCodificar
+            printChar(currentLetterIndex + 1, wordArray);
+        });
+}
