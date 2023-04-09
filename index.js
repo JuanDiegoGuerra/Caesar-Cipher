f54d83d5f268fddc6d4bf447eaeda2be43ef59ee
Juan Diego Guerra <jdxelric@gmail.com>
2023-04-08 22:50:23 -0500

    Update index.js
    Added primary consts

---
+++ index.js
@@ -0,0 +1,5 @@
+const alfabeto = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const inputOriginal = document.getElementById('input-original');
+const cifrador = document.getElementById('cifrador');
+const resultado = document.getElementById('resultado');
+const rango = document.getElementById('rango');
